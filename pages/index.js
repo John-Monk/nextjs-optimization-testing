a784211f290c344940a03d901d6a0f924a8e47ff
John Monk <johntmonk1@gmail.com>
2023-01-18 11:35:16 -0600

    disabled lazy loading for images, added filler section to index.js, added media query to correct background hero image on mobile

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import logo from '../public/Roofer-RoofingLogo.svg';
 import background from '../public/hero-image.webp';
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
+import Card from '@/components/Card';
 
 export default function Home() {
   const [transition, setTransition] = useState(false);
@@ -23,7 +24,7 @@ export default function Home() {
       </Head>
       <section className="hero section">
         <div className={`container content ${transition && 'show'}`}>
-          <Image className="logo" src={logo} />
+          <Image className="logo" src={logo} priority/>
           <h1>Fino Exterior</h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
@@ -54,10 +55,28 @@ export default function Home() {
         </picture> */}
         <div className="background">
           <div className="overlay"></div>
-          <Image src="/hero-image.webp" layout={'fill'} objectFit={'cover'} />
+          <Image src="/hero-image.webp" layout={'fill'} objectFit={'cover'} priority/>
         </div>
       </section>
       <section className="section-two">
+        <div className="side left"></div>
+        <div className="main"></div>
+        <div className="side right"></div>
+      </section>
+      <section className="section-three">
+        <div class="custom-shape-divider-top-1674028994">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+              class="shape-fill"
+            ></path>
+          </svg>
+        </div>
         <h1>Section</h1>
       </section>
     </Wrapper>
@@ -78,7 +97,7 @@ const Wrapper = styled.div`
     clip-path: inset(0 0 0 0);
 
     .background {
-      position: fixed;
+      position: absolute;
       height: 100%;
       width: 100%;
       left: 0;
@@ -158,9 +177,58 @@ const Wrapper = styled.div`
     align-items: center;
     z-index: 1;
 
+    div {
+      background-color: white;
+      -webkit-box-shadow: 0px 0px 17px -4px rgba(0, 0, 0, 0.75);
+      -moz-box-shadow: 0px 0px 17px -4px rgba(0, 0, 0, 0.75);
+      box-shadow: 0px 0px 17px -4px rgba(0, 0, 0, 0.75);
+    }
+
+    .side {
+      padding: 11em 9em;
+    }
+
+    .left {
+      transform: translateX(2em);
+    }
+
+    .right {
+      transform: translateX(-2em);
+    }
+
+    .main {
+      padding: 13em 11em;
+      z-index: 1;
+    }
+
     h1 {
       font-size: 5rem;
       color: ${(props) => props.theme.colors.white};
+    }
+  }
+
+  .section-three {
+    background: ${(props) => props.theme.colors.white};
+    height: 100vh;
+
+    /* .custom-shape-divider-top-1674028994 {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      overflow: hidden;
+      line-height: 0;
+    }
+
+    .custom-shape-divider-top-1674028994 svg {
+      position: relative;
+      display: block;
+      width: calc(142% + 1.3px);
+      height: 327px;
+    } */
+
+    .custom-shape-divider-top-1674028994 .shape-fill {
+      fill: ${(props) => props.theme.colors.orange};
     }
   }
 
@@ -173,6 +241,10 @@ const Wrapper = styled.div`
   @media screen and (min-width: 860px) {
     .hero {
       background-attachment: fixed;
+
+      .background {
+        position: fixed;
+      }
 
       .container {
         width: unset;
